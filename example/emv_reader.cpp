9e761ef469243863814b6db4d5a4c580c9e1f7c9
edbc <edbc>
2025-02-23 00:56:53 -0400

    update

--- example/emv_reader.cpp
+++ example/emv_reader.cpp
@@ -7,24 +7,33 @@ using namespace nodepp;
 
 void onMain() {
 
-    if( !fs::exists_file( path::join( "/dev", process::env::get("DEVICE") ) ) )
-      { process::error( process::env::get("DEVICE"), "device not found" ); return; }
+    auto dir = "/dev/" + process::env::get("DEVICE");
 
-    serial_t str ( process::env::get("DEVICE"), 115200 );
+    if( process::env::get("DEVICE").empty() ){ goto ERROR; }
+    if( !fs::exists_file( dir ) )            { goto ERROR; }
 
-    cin::onData([=]( string_t data ){
-        str.write( encoder::buffer::hex2buff( data.slice( 0, -1 ) ) );
-    });
+    do {
 
-    str::onData([=]( string_t data ){
-        console::log( "->", encoder::buffer::buff2hex( data ) );
-    });
+        auto str = serial_t( dir, 115200 );
+        auto cin = fs::std_input();
 
-    str::onClose([=](){
-        console::log(" Device Disconnected ");
-    }); console::log(" Device Connected ");
+        cin.onData([=]( string_t data ){
+            auto hex = regex::match_all( data, "\\w+" );
+            str.write( encoder::buffer::hex2buff( hex.join("") ) );
+        });
 
-    stream::pipe( cin );
-    stream::pipe( str );
+        str.onData([=]( string_t data ){
+            console::log( "->", encoder::buffer::buff2hex( data ) );
+        });
+
+        str.onClose([=](){
+            console::log(" Device Disconnected ");
+        }); console::log(" Device Connected ");
+
+        stream::pipe( cin ); stream::pipe( str );
+
+    } while(0); return; ERROR:;
+
+    process::error( process::env::get("DEVICE"), "device not found" );
 
 }
